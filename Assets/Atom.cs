af0b3ed8924955dd1ce75cc07a405e6cbfe5802e
Vinicius Vendramini <vinivendra@gmail.com>
2017-06-28 11:49:57 -0300

    Added initial atom logic.

--- Assets/Atom.cs
+++ Assets/Atom.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Atom : MonoBehaviour {
 
-	public GameObject rhand;
-	public GameObject lhand;
+	public static float minimumConnectionProximity = 2.5f;
+
+	public Hand rhand;
+	public Hand lhand;
 	public Material blue;
 	public Material yellow;
 	public Material red;
@@ -15,6 +17,7 @@ public class Atom : MonoBehaviour {
 	private bool isClosestToRightHand;
 	private bool isClosestToLeftHand;
 
+	private Atom parent;
 	private List<Atom> children;
 
 	public static List<Atom> allAtoms = new List<Atom> ();
@@ -39,6 +42,7 @@ public class Atom : MonoBehaviour {
 		if (isBeingHeld) {
 			GetComponent<Renderer> ().material = red;
 		} else {
+			attemptToCreateNewConnection ();
 			UpdateHighlights ();
 		}
 	}
@@ -55,6 +59,48 @@ public class Atom : MonoBehaviour {
 				UpdateHighlights ();
 			}
 		}
+	}
+
+	private void attemptToCreateNewConnection () {
+		Atom otherAtom = getOtherHeldAtom ();
+		if (otherAtom == null) {
+			return;
+		}
+
+		float distance = Vector3.Distance (transform.position, otherAtom.transform.position);
+		if (distance <= minimumConnectionProximity) {
+			otherAtom.setParent (this);
+			otherAtom.transform.SetParent (this.transform, true);
+			children.Add (otherAtom);
+		}
+	}
+
+	private void setParent(Atom newParent) {
+		if (parent != null) {
+			parent.setParent (this);
+			children.Add (parent);
+		}
+		this.parent = newParent;
+		parent.transform.SetParent (this.transform, true);
+	}
+
+	private Atom getOtherHeldAtom () {
+		Atom otherAtom = null;
+		if (rhand.atom != null && (rhand.atom.GetInstanceID () != this.gameObject.GetInstanceID ())) {
+			print ("Other is right");
+			print (rhand.atom);
+			print (rhand.atom.GetInstanceID ());
+			print (this.GetInstanceID ());
+			otherAtom = rhand.atom.GetComponent<Atom> ();
+		} else if (lhand.atom != null && (lhand.atom.GetInstanceID () != this.gameObject.GetInstanceID ())) {
+			print ("Other is left");
+			print (lhand.atom);
+			print (this);
+			print (lhand.atom.GetInstanceID ());
+			print (this.GetInstanceID ());
+			otherAtom = lhand.atom.GetComponent<Atom> ();
+		}
+		return otherAtom;
 	}
 
 	void UpdateHighlights () {
