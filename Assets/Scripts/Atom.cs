d6a63075536386311c4b2ddf8720c977fcc781c8
Cadu Elmadjian <cadu_elmadjian@yahoo.com.br>
2017-06-28 20:35:44 -0300

    case 1 + prefabs

---
+++ Assets/Scripts/Atom.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atom : MonoBehaviour {
+
+	public GameObject rhand;
+	public GameObject lhand;
+	public GameObject molecule;
+	public Material selected;
+	public Material unselected;
+	private float proximity;
+	private Behaviour halo;
+
+	// Use this for initialization
+	void Start () {
+		proximity = 3.8f;
+		halo = (Behaviour)GetComponent ("Halo");
+		halo.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		bool rhandState = Manipulate (rhand);
+		bool lhandState = Manipulate (lhand);
+		if (!rhandState && !lhandState) {
+			GetComponent<Renderer> ().material = unselected;
+			halo.enabled = false;
+		}
+	}
+
+	private bool Manipulate(GameObject hand) {
+		Transform ht = hand.GetComponent<Transform> ();
+		//check distance
+		if (Vector3.Distance (ht.position, transform.position) < proximity) {
+			halo.enabled = true;
+			//GetComponent<Renderer>().material = selected;
+			Hand hstate = hand.GetComponent<Hand> ();
+			Molecule mol = molecule.GetComponent<Molecule> ();
+			//get the atom
+			if (hstate.pinch && (hstate.atom == null || hstate.atom == gameObject) && !mol.selected) {
+				GetComponent<Renderer> ().material = selected;
+				transform.position = ht.position;
+				transform.rotation = ht.rotation;
+				hstate.atom = gameObject;
+				//release the atom
+			} else {
+				hstate.atom = null;
+			}
+			return true;
+		}
+		return false;
+	}
+		
+//	void OnTriggerEnter(Collider col) {
+//		if (col.tag == "atom") {
+//			GameObject mol = col.gameObject.GetComponent<Atom> ().molecule;
+//
+//			//there's no molecule yet
+//			if (mol == null) {
+//				mol = new GameObject ();
+//				mol.name = "Molecule";
+//				col.gameObject.transform.SetParent (mol.GetComponent<Transform>());
+//				col.gameObject.GetComponent<Atom> ().molecule = mol;
+//			}
+//
+//			//make the atom join the molecule
+//			if (molecule != null) {
+//				transform.SetParent (mol.GetComponent<Transform>());
+//				molecule = mol;
+//			}
+//		}
+//	}
+}
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System;
+using System.Globalization;
+using System.Text;
+
+public class Hand : MonoBehaviour {
+	
+	public Material selected;
+	public Material unselected;
+	public bool pinch;
+	public GameObject atom;
+	public bool isRightHand;
+
+	List<String> commands;
+
+	private Vector3 up = new Vector3 (0, 1, 0);
+	private Vector3 forward = new Vector3 (0, 0, 1);
+	private Vector3 right = new Vector3 (1, 0, 0);
+
+	private bool pinchIsPressed = false;
+
+	// Use this for initialization
+	void Start () {
+		pinch = false;
+		atom = null;
+
+		if (isRightHand) {
+			commands = RightHandCommands ();
+		} else {
+			commands = LeftHandCommands ();
+		}
+	}
+
+	public void ParseData(string[] data) {
+		if (data != null) {
+			string[] pos_data = data [1].Split (',');
+			string[] rot_data = data [2].Split (',');
+			string pinchState = data [3];
+			SetPosition (pos_data);
+			SetRotation (rot_data);
+			SetPinch (pinchState);
+		}
+	}
+
+	private void SetPosition(string[] pos_data) {
+		float x = Convert.ToSingle (pos_data [0], CultureInfo.InvariantCulture);
+		float y = Convert.ToSingle (pos_data [1], CultureInfo.InvariantCulture);
+		float z = Convert.ToSingle (pos_data [2], CultureInfo.InvariantCulture);
+		Vector3 pos = new Vector3 (x, y, z);
+		transform.localPosition = pos;
+	}
+
+	private void SetRotation(string[] rot_data) {
+		float x = Convert.ToSingle (rot_data [0], CultureInfo.InvariantCulture);
+		float y = Convert.ToSingle (rot_data [1], CultureInfo.InvariantCulture);
+		float z = Convert.ToSingle (rot_data [2], CultureInfo.InvariantCulture);
+		Quaternion target = Quaternion.Euler(x, y, z);
+		transform.rotation = target;
+	} 
+
+	private void SetPinch(string pinchState) {
+		SetPinch(pinchState == "true");
+	}
+
+	private void SetPinch(bool pinchState) {
+		if (pinchState) {
+			GetComponent<Renderer> ().material = selected;
+			pinch = true;
+		} else {
+			GetComponent<Renderer> ().material = unselected;
+			pinch = false;
+		}
+	}
+
+	void Update () {
+		float translationSpeed = 0.1f;
+		float rotationSpeed = 3.0f;
+
+		// Translation
+		if (!Input.GetKey ("space")) {
+			if (Input.GetKey (commands [0])) {
+				transform.Translate (forward * translationSpeed);
+			}
+			if (Input.GetKey (commands [1])) {
+				transform.Translate (-forward * translationSpeed);
+			}
+			if (Input.GetKey (commands [2])) {
+				transform.Translate (right * translationSpeed);
+			}
+			if (Input.GetKey (commands [3])) {
+				transform.Translate (-right * translationSpeed);
+			}
+			if (Input.GetKey (commands [4])) {
+				transform.Translate (up * translationSpeed);
+			}
+			if (Input.GetKey (commands [5])) {
+				transform.Translate (-up * translationSpeed);
+			}
+		}
+		// Rotation
+		else {
+			if (Input.GetKey (commands [0])) {
+				transform.Rotate (new Vector3 (rotationSpeed, 0, 0));
+			}
+			if (Input.GetKey (commands [1])) {
+				transform.Rotate (new Vector3 (-rotationSpeed, 0, 0));
+			}
+			if (Input.GetKey (commands [2])) {
+				transform.Rotate (new Vector3 (0, 0, rotationSpeed));
+			}
+			if (Input.GetKey (commands [3])) {
+				transform.Rotate (new Vector3 (0, 0, -rotationSpeed));
+			}
+			if (Input.GetKey (commands [6])) {
+				transform.Rotate (new Vector3 (0, rotationSpeed, 0));
+			}
+			if (Input.GetKey (commands [7])) {
+				transform.Rotate (new Vector3 (0, -rotationSpeed, 0));
+			}
+		}
+			
+		// Pinch
+		if (Input.GetKey (commands [8])) {
+			if (!pinchIsPressed) {
+				SetPinch (!pinch);
+			}
+			pinchIsPressed = true;
+		} else {
+			pinchIsPressed = false;
+		}
+	}
+
+	List<String> LeftHandCommands() {
+		List<String> commands = new List<String> ();
+		commands.Add ("w");
+		commands.Add ("s");
+		commands.Add ("d");
+		commands.Add ("a");
+		commands.Add ("2");
+		commands.Add ("x");
+		commands.Add ("q");
+		commands.Add ("e");
+		commands.Add ("z");
+		return commands;
+	}
+
+	List<String> RightHandCommands() {
+		List<String> commands = new List<String> ();
+		commands.Add ("i");
+		commands.Add ("k");
+		commands.Add ("l");
+		commands.Add ("j");
+		commands.Add ("8");
+		commands.Add (",");
+		commands.Add ("u");
+		commands.Add ("o");
+		commands.Add (".");
+		return commands;
+	}
+}
